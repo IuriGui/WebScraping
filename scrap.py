9678fa570bb1be2e41d1143fd05413d17dd913db
Iuri <iuriguilhermesantos@gmail.com>
2025-10-14 22:50:14 -0300

    first commit

--- scrap.py
+++ scrap.py
@@ -1,6 +1,8 @@
 import re
 import time
 import random
+from urllib.error import HTTPError, URLError
+from urllib.parse import urljoin
 from urllib.request import urlopen
 
 
@@ -8,63 +10,126 @@ from bs4 import BeautifulSoup
 from urllib3.util import url
 
 
+URL_BASE = "https://books.toscrape.com/"
 
 
-#time.sleep(random.uniform(2, 5))
+
+def get_bs(url):
+    """Acessa a url, retorna o objeto beatifulSoup e agurda o tempinho pela educação"""
+    try:
+        html = urlopen(url)
+        bs_obj = BeautifulSoup(html, "html.parser")
+        #time.sleep(random.uniform(2, 5))
+        return bs_obj
+    except HTTPError as e:
+        print(e)
+    except URLError as e:
+        print("Erro na url")
+    return None
 
 
-def getCategoryLinks(url):
-   html = urlopen(url)
-   links = set()
-   bs = BeautifulSoup(html.read(), 'html.parser')
-   categorias = bs.find("ul", {"class": "nav nav-list"}).find_all("li")
-   for categoria in categorias:
-       link = categoria.find("a").get("href")
-       links.add(url + link)
+def get_categories(url):
+    """Acessa a lista de categorias e retorna os links delas"""
+
+    bs = get_bs(url)
+
+    ##Lista de retorno
+    categories_link_list = set()
+
+    try:
+
+        categories_list = bs.find("ul", {"class": "nav nav-list"})
+
+        if not categories_list:
+            print("Não foi possível encontrar a lista de categorias")
+            return None
+
+        categories_list = categories_list.find("ul").find_all("li")
+
+        if not categories_list:
+            print("Não foi possível acessar a lista de categorias")
 
 
-   return links
+        for category in categories_list:
+            link_tag = category.find("a")
+            if not link_tag:
+                continue
+            href = link_tag.get("href")
+            if not href:
+                continue
+            categories_link_list.add(URL_BASE + href)
+
+        return categories_link_list
+
+    except AttributeError as e:
+        print("Tag não encontrada")
+        return None
+    except Exception as e:
+        print(e)
+        return None
 
 
-def getBookLinks(url):
-   if(url == None):
-       return None
-   html = urlopen(url)
+def get_books(url, list_of_books_links=None):
+    """Retorna todos os livros de uma página de categoria"""
+
+    if url is None:
+        return list_of_books_links
+
+    if list_of_books_links is None:
+        list_of_books_links = set()
+
+    bs = get_bs(url)
+
+    try:
+        print("Pegando livros...   " )
+        books_section = bs.find("section")
+        if not books_section:
+            print("Não foi possível encortrar a seção de livros")
+            return None
+
+        list_of_books = books_section.find("ol").find_all("li")
+        if not list_of_books:
+            print("Não foi possível acessar a lista de livros")
+            return None
+
+        for book in list_of_books:
+            link_tag = book.find("a")
+            if not link_tag:
+                print("Tag <a> não encontrada")
+                continue
+            href = link_tag.get("href")
+            if not href:
+                print("Href não encontrado")
+                continue
+            href = href.replace("../../..", "")
+            list_of_books_links.add(URL_BASE + "catalogue" + href)
+
+        link_next_page = bs.find("li", class_="next")
+        if not link_next_page:
+            print("Esta categoria não possui mais páginas")
+            return None
+
+        link_next_page = link_next_page.find("a").get("href")
+
+        if link_next_page:
+            new_url = re.sub(r'[^/]+$', link_next_page, url)
+            #print(link_next_page)
+            get_books(new_url, list_of_books_links)
+
+        return list_of_books_links
+
+    except AttributeError as e:
+        print(e)
+        return None
 
 
-   html = urlopen(url)
+
+def main():
+    categories_link = get_categories(URL_BASE)
 
 
-   bs = BeautifulSoup(html.read(), 'html.parser')
-   links = set()
 
+fd = "https://books.toscrape.com/catalogue/wall-and-piece_971/index.html"
 
-   padrao = re.compile(r'/[A-Za-z0-9_-]+/index\.html')
+ls = get_bs(fd)
 
-
-   t = bs.find("ol")
-   for li in t.find_all("li"):
-       link = li.find("a").get("href")
-       link = padrao.search(link)
-       li = "https://books.toscrape.com/catalogue" + link.group()
-       links.add(li)
-
-
-   return links
-
-
-def getBooksLinkInCategory(links_category):
-headers = {
-   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-    # Ou, para ser transparente:
-  # 'User-Agent': 'Projeto Academico de Web Scraping para a disciplina de Análise de Dados e Extração de Conhecimento - Universidade Federal de Santa maria - Contato: seu.email@dominio.com'
-}
-##<ul class="nav nav-list"> lista de categorias
-
-
-#link_categorias = getCategoryLinks("https://books.toscrape.com/")
-
-links_categorias = getCategoryLinks("https://books.toscrape.com/")
-links_books = getBookLinks("https://books.toscrape.com/")
-for link in links_books:
-   print(link)
